de9ac247870bb366ef2d9e90de69f1709ebc32c6
Collins Bawa <collinskwamebawa@yahoo.com>
2023-05-16 15:08:31 +0000

    add reducers and actions

---
+++ src/redux/categories/categoriesSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState: 'Under construction',
+  reducers: {
+    // No additional reducers needed
+  },
+});
+
+export default categoriesSlice.reducer;
